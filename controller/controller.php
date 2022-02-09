7db91e626235ff2464deeb4f36c8444e518464e7
Noria Naneche <noriananeche@hotmail.com>
2022-02-09 11:46:20 +0100

    majuscule pour la classe model

--- controller/controller.php
+++ controller/controller.php
@@ -2,7 +2,7 @@
 require("./model/model.php");
 
 function viewProducts() {
-    $model= new model();
+    $model= new Model();
     $result= $model->view();
     require("./view/list.php");
 }
--- model/model.php
+++ model/model.php
@@ -1,6 +1,6 @@
 <?php
 
-class model 
+class Model 
 {
 private function connect(){
         try {
